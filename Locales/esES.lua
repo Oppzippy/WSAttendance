c7f5130512678bc39316b82c0c833154a14d4e63
Kyle Scheuing <oppzippy@gmail.com>
2019-05-13 20:36:06 -0400

    Update locales

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -1,3 +1,6 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("WSAttendance", "esES")
     or LibStub("AceLocale-3.0"):NewLocale("WSAttendance", "esMX")
 if not L then return end
+
+L.yes = YES
+L.no = NO
--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -1,2 +1,5 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("WSAttendance", "frFR")
 if not L then return end
+
+L.yes = YES
+L.no = NO
